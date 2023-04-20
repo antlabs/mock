a48d3ee8b716bdcc74fc28b75b85d135789496bd
guonaihong <guonaihong@qq.com>
2023-04-21 00:24:19 +0800

    Mac (#2)
    * add mac地址
    
    * fix错字

---
+++ mac/mac.go
@@ -0,0 +1,16 @@
+package mac
+
+import (
+	"math/rand"
+	"net"
+)
+
+// mac 地址
+// 目前仅支持mac 48格式的生成
+func Mac() string {
+	mac := make([]byte, 6)
+	rand.Read(mac)
+	// net.HardwareAddr比fmt.Sprintf更快
+	// return fmt.Sprintf("%02x:%02x:%02x:%02x:%02x:%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5])
+	return net.HardwareAddr(mac).String()
+}
